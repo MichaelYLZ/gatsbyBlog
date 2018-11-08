648c402d4eecf7b040c47688d5bd26b5541eacc0
MichaelYLZ <liyang_shifan@163.com>
2018-11-08 11:25:55 +0800

    加了一些图片进去-第二次commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,10 @@ import Header from "../components/header"
 
 export default () => (
 	<div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello Gatsby!" />
+    <Link to="/contact/">联系方式</Link>
+    <br />
+    <Link to="/pics/">图片</Link>
+    <Header headerText="Hello everyone!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
