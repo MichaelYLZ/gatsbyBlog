51b370ccf2cf2a724a00c32407365e040e7f9cbb
MichaelYLZ <liyang_shifan@163.com>
2018-11-08 11:37:12 +0800

    加入图片文件夹 第三次commit

---
+++ src/pages/pics.js
@@ -0,0 +1,33 @@
+import React from "react"
+import pic01 from "../pics/01.png"
+import pic02 from "../pics/02.png"
+import pic03 from "../pics/03.png"
+import pic04 from "../pics/04.png"
+
+export default () => (
+  <div>
+    <p>
+    南京迪卡侬购物
+    <br />
+      <img src={pic01}/>
+    </p>
+    
+    <p>
+    新街口-高逼格西餐厅-Cobb Salad-考博色拉
+    <br />
+      <img src={pic02}/>
+    </p>
+    
+     <p>
+    德国朋友
+    <br />
+      <img src={pic03}/>
+    </p>
+    
+     <p>
+   猫咪
+    <br />
+      <img src={pic04}/>
+    </p>
+  </div>
+)
